f9786072e39de17c840fdd6f59735f6b206041cf
Kibo no shirei-kan <kibo.no.shirei.kandev@tutamail.com>
2024-05-22 13:56:02 +0800

    Main fucntion takes args

---  main.c
+++  main.c
@@ -1,3 +1,15 @@
 #include studio.h
 
-main {
+#include <stdio.h>
+
+void emptyFunction(int argc, char *argv[]) {
+    // This function does nothing with the arguments
+    // You can add code here to do something with the arguments if needed
+}
+
+int main(int argc, char *argv[]) {
+    // Call the emptyFunction and pass the arguments to it
+    emptyFunction(argc, argv);
+
+    return 0;
+}
