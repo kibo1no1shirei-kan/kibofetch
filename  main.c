0adb621e2975ada172a3d9529d982f65154e080e
Kibo no shirei-kan <kibo.no.shirei.kandev@tutamail.com>
2024-05-26 17:38:05 +0800

    day1

---  main.c
+++  main.c
@@ -1,17 +1,21 @@
 
 #include <stdio.h>
 
-void emptyFunction(int argc, char *argv[]) {
-     for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-        printf("dd");
-    }
 
-}
+int kibofetchcheck(); 
+
 
 int main(int argc, char *argv[]) {
-    // Call the emptyFunction and pass the arguments to it
-    emptyFunction(argc, argv);
+    if (argc == 1){
+        kibofetchcheck();
+           
+    }
 
     return 0;
+} 
+
+
+int kibofetchcheck(){
+
+
 }
